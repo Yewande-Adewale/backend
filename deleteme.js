5a4119a2de7c3d395c84337b054c1e1009c82de9
Yewande Adewale <133135714+Yewande-Adewale@users.noreply.github.com>
2023-05-11 14:37:03 +0100

    Add files via upload

--- deleteme.js
+++ deleteme.js
@@ -1,7 +1,9 @@
-const fs= require ("fs");
-fs.unlink("./sample.txt",(error)=>{
-    if(error){
-        throw error;
-    }
-    console.log("Delete a File Successfully")
+const fs=require("fs").promises;
+fs.unlink("./sample.txt","delete a file","utf8")
+.then(()=>{
+    console.log("File has been deleted")
+})
+.catch((error)=>{
+    console.log(error);
 });
+
