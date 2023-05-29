2a5deaf37bc7b9965f7a4c9016c9d2c4d86ab35c
Yewande Adewale <133135714+Yewande-Adewale@users.noreply.github.com>
2023-05-29 23:07:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,108 +2,104 @@ const express = require('express' )
 const PORT = 7000;
 
 const app = express();
-app.use( express.json() )
+app.use( express.json())
 
-const weeklyTask = [
-    {
-        id: 1,
-        Days: "Monday",
-        Presentation: "Bio Report",
-        Date: "May 09",
-        Time: "3pm",
-        Location: "Ikeja"
+    const employmentSystem= [
+            {
+                id: 1,
+                staffName: "Grace",
+                staffAge: 27,
+                staffSalary: 4000,
+                staffPosition: "supervisor",
+                staffQualification: "Bsc",
+                staffGender: "female"
+            },
+            {
+                id: 2,
+                staffName: "Peter",
+                staffAge: 30,
+                staffSalary: 3000,
+                staffPosition: "instructor",
+                staffQualification: "Hnd",
+                staffGender: "male"
+            },
+    ]
 
-    },
-
-   {    id: 2,
-        Days: "Thursday",
-        Presentation: "Drone Exhibition",
-        Date: "May 13",
-        Time: "10am",
-        Location: "Airport"
-   }
-]
-
-//welcome
-app.get('/', (req, res) => {
-    res.status( 200 ).json( {
-        message: "Welcome to the todo list"
-    })
-});
-
-//showing all tasks for the week
-app.get('/taskRecords', (req, res) => {
-        res.status( 200 ).json({
-             data: weeklyTask
-         })
-         // res.send(task)
-     } );
-
-    
-    //one task for the week
-app.get( '/taskRecords/:id', ( req, res ) => {
-    const taskId = parseInt( req.params.id );
-    const task = weeklyTask.find( ( item ) => ( item.id === taskId ) );
-    if ( !task ) {
-        res.status( 404 ).json( {
-            data: "task not found."
-    
+    app.get( '/', (req, res) => {
+        res.status( 200 ).json( {
+            message: "Welcome to the Employment System"
         })
-    } else {
-        res.status( 200 ).json({
-            data: task
+    } );
+    
+    app.get( '/employmentRecords', (req, res) => {
+        res.status( 200 ).json( {
+            data: employmentSystem
+        })
+    } );
+    app.get( '/employmentRecords/:id', ( req, res ) => {
+        const staffId = parseInt( req.params.id );
+        const staff = employmentSystem.find( ( item ) => ( item.id === staffId ) );
+        if ( !staff ) {
+            res.status( 404 ).json( {
+                data: "user not found."
+        
+            })
+        } else {
+            res.status( 200 ).json({
+                data: staff
+            })
+        }
+        })
+
+app.post('/employmentRecords', (req,res) =>{
+    const newStaff = req.body;
+     newStaff.id = employmentSystem.length +1;
+     weeklyTask.push(newStaff);
+     res.status( 200 ).json({
+         data: newStaff
+        });
+    
+     })
+
+//
+app.patch('/employmentRecords/:id', function(req,res){
+    const staffId = parseInt(req.params.id);
+    const updatedStaff= req.body;
+    const index = employmentSystem.findIndex((item) => (item.id === staffId))
+    if(index !== -1){
+        employmentSystem[index] ={ ...employmentSystem[index], ...updatedStaff}
+        res.status(200).json({
+            new: employmentSystem[index]
+        })
+    }else{
+        res.send("Wrong id sent")
+    }
+})
+
+
+ app.delete("/employmentRecords/:id", (req,res)=>{
+    const staffId = parseInt(req.params.id)
+    const index = employmentSystem.findIndex((item)=>(item.id === staffId))
+    if (!index){
+        res.status(404).json({
+            message: `This id: ${staffId} does not exist`
+
+        })
+    }else{
+        deletedstaff = employmentSystem[index]
+        employmentSystem.splice(index, 1)
+        res.status(200).json({
+            deletedData: deletedstaff
         })
     }
-    })
-
-    app.post('/taskRecords', (req,res) =>{
-        const newTask = req.body;
-         newTask.id = weeklyTask.length +1;
-         weeklyTask.push(newTask);
-         res.status( 200 ).json({
-             data: newTask
-            });
-        
-         })
-
-   //
-    app.patch('/taskRecords/:id', function(req,res){
-        const taskId = parseInt(req.params.id);
-        const updatedTask = req.body;
-        const index = weeklyTask.findIndex((item) => (item.id === taskId))
-        if(index !== -1){
-            weeklyTask[index] ={ ...weeklyTask[index], ...updatedTask}
-            res.status(200).json({
-                new: weeklyTask[index]
-            })
-        }else{
-            res.send("Wrong id sent")
-        }
-    })
+ })
 
 
-     app.delete("/taskRecords/:id", (req,res)=>{
-        const taskId = parseInt(req.params.id)
-        const index = weeklyTask.findIndex((item)=>(item.id === taskId))
-        if (!index){
-            res.status(404).json({
-                message: `This id: ${taskId} does not exist`
-    
-            })
-        }else{
-            deletedTask = weeklyTask[index]
-            weeklyTask.splice(index, 1)
-            res.status(200).json({
-                deletedData: deletedTask
-            })
-        }
-     })
-    
-    
 
 
 app.listen( PORT, () => {
-    console.log("Sever is listening to port " + PORT)
+console.log("Sever is listening to port " + PORT)
 })
+
 
 
